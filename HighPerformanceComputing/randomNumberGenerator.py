cc86be0c5f4fba758aa1598a6c689f908f5ce621
Zaighum <zaighum47@gmail.com>
2014-06-24 23:26:52 +0200

    removing plot

--- HighPerformanceComputing/randomNumberGenerator.py
+++ HighPerformanceComputing/randomNumberGenerator.py
@@ -2,4 +2,3 @@ from numpy.random import RandomState
 import matplotlib.pyplot as plt
 random_generator = RandomState(seed=55)
 r = random_generator.uniform(0., 1., size=200)
-plt.plot(r[::2], r[1::2], 'bo')
