0881aaec37b342abdceda6ed191d691470512c22
Zaighum Rajput <zaighum47@gmail.com>
2014-07-11 13:30:00 +0200

    qt

---
+++ qT/alarm.py
@@ -0,0 +1,35 @@
+'''
+Rapid GUI Programming with Python and Qt
+'''
+
+import sys #needed for sys arguments
+import time #needed for sleep
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
+
+app = QApplication(sys.argv) #comes as a list
+
+try:
+	due = QTime.currentTime()
+	print due
+	message = "Alert!"
+	if len(sys.argv) < 2:
+		raise ValueError
+	hours, mins = sys.argv[1].split(":")
+	due = QTime(int(hours), int(mins))
+	if not due.isValid():
+		raise ValueError
+	if len(sys.argv) > 2:
+		message = " ".join(sys.argv[2:])
+except ValueError:
+	message = "To use alarm.py HH:MM [optional message]" #24hr clock
+	
+
+while QTime.currentTime() < due:
+	time.sleep(20) # 20 seconds
+	
+label = QLabel("<font color=red size=72><b>" + message + "</b></font>")
+label.setWindowFlags(Qt.SplashScreen)
+label.show()
+QTimer.singleShot(60000, app.quit) #1 minute
+app.exec_()
