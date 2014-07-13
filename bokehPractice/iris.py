36d95613559d199c2434584c706ab5a3d23bd182
Zaighum <zaighum47@gmail.com>
2014-07-13 23:27:21 +0200

    copying some examples

---
+++ bokehPractice/iris.py
@@ -0,0 +1,19 @@
+#example from
+
+from bokeh.sampledata.iris import flowers
+from bokeh.plotting import *
+
+output_file("iris.html", title="iris.py example")
+
+colormap = {'setosa': 'blue', 'versicolor': 'green', 'virginica': 'blue'}
+
+flowers['color'] = flowers['species'].map(lambda x: colormap[x])
+
+#setting the name kwarg will give this scatter plot a user
+#friendly id, and the corresponding embed.js will have a nice name
+#too
+
+scatter(flowers["petal_length"], flowers["petal_width"],
+        color=flowers["color"], fill_alpha=0.2, size=10, name="iris")
+
+show()
