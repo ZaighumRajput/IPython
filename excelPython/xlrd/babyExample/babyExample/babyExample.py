1cc1c1d35e38fcde6a48043fcc2f627bc3594e0b
zaighumrajput <zaighum47@gmail.com>
2014-11-06 16:24:01 +0100

    made a python list, seems convenient enough

--- excelPython/xlrd/babyExample/babyExample/babyExample.py
+++ excelPython/xlrd/babyExample/babyExample/babyExample.py
@@ -5,4 +5,10 @@ excelFile = "data/someData.xlsx"
 workbook = xlrd.open_workbook(excelFile)
 sheet = workbook.sheet_by_index(0)
 
-print sheet.cell_value(2,0)
+#shove it in a list to make it easier
+data = [[sheet.cell_value(r,col)
+         for col in range(sheet.ncols)]
+            for r in range(sheet.nrows)]
+
+
+print data[0][0]
